7ee87fb0c47c56fb8a928f3bef9641f2f6c8d38d
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-27 15:31:10 -0500

    Work in Progress

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,18 @@
-def char_count(str):
-  # Your code here
+def char_count(param):
+  dictionary = {}
+
+  print(param)
+
+  for char in param.lower():
+    if char != ' ':
+      if char in dictionary:
+        dictionary[char]+=1
+      else:
+        dictionary[char] = 1
+
+  final_count = [[k, v] for k,v in dictionary.items()]
+
+  return final_count
+
+
+print(char_count('an apple a day will keep the doctor away'))
