f161f88989008d3b8f260a4bd88b706356c350ab
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-27 23:07:39 -0500

    Completed algo

--- python/char_count.py
+++ python/char_count.py
@@ -1,8 +1,9 @@
 def char_count(param):
   dictionary = {}
 
-  print(param)
-
+  # Loops through each letter in string if it isn't a space
+  # if that character doesn't exist in the dictionary, add it and set its value to 1
+  # if that character does already exist in the dictionary, increment its value
   for char in param.lower():
     if char != ' ':
       if char in dictionary:
@@ -10,9 +11,8 @@ def char_count(param):
       else:
         dictionary[char] = 1
 
-  final_count = [[k, v] for k,v in dictionary.items()]
+  # Creates a list of key-value pair lists
+  # In this list comp, the dictionary is first sorted alphabetically by its keys, then is sorted descendingly by value
+  final_count = [[k, v] for k,v in sorted(sorted(dictionary.items()), key=lambda item: item[1], reverse=True)]
 
   return final_count
-
-
-print(char_count('an apple a day will keep the doctor away'))
