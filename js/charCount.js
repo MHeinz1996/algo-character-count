51bb354b048dae63349fb8069e6b5ab7a16a6459
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-28 00:07:47 -0500

    Work in progress

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,25 @@
-exports.charCount = function() {
+exports.charCount = function(param) {
+    let myObj = {};
+    let char = param.split('');
 
+    for(let i=0; i<char.length; i++) {
+        if(char[i] != ' ') {
+            if(char[i] in myObj) {
+                myObj[char[i]]+=1;
+            } else {
+                myObj[char[i]] = 1;
+            }
+        }
+    }
+
+    let final_count = []
+
+    for(let prop in myObj){
+        // console.log(`${prop} : ${myObj[prop]}`);
+        final_count.push([prop, myObj[prop]])
+    }
+
+    return final_count.sort(function(a,b){return b[1] - a[1]});
 };
+
+console.log(exports.charCount('an apple a day will keep the doctor away'));
