dd8ca9fd0d54f7b0eb388a5faf65e5113414d04f
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-28 00:34:41 -0500

    Completed algo

--- js/charCount.js
+++ js/charCount.js
@@ -2,6 +2,7 @@ exports.charCount = function(param) {
     let myObj = {};
     let char = param.split('');
 
+    // Loops through each letter in string if it isn't a space
     for(let i=0; i<char.length; i++) {
         if(char[i] != ' ') {
             if(char[i] in myObj) {
@@ -12,14 +13,29 @@ exports.charCount = function(param) {
         }
     }
 
-    let final_count = []
+    let finalCount = []
 
-    for(let prop in myObj){
+    // Orders myObj alphabetically by key
+    let sorted = Object.keys(myObj).sort().reduce(
+        (obj, key) => { 
+          obj[key] = myObj[key]; 
+          return obj;
+        }, 
+        {}
+      );
+
+    // Pushes key-value pairs into an array of key-value pairs
+    for(let prop in sorted){
         // console.log(`${prop} : ${myObj[prop]}`);
-        final_count.push([prop, myObj[prop]])
+        finalCount.push([prop, sorted[prop]]);
     }
 
-    return final_count.sort(function(a,b){return b[1] - a[1]});
-};
+    // Sorts array by value descendingly
+    let orderedCount = finalCount.slice().sort(function(a,b){
+        if(a[1] > b[1]) {return -1};
+        if(a[1] < b[1]) {return 1};
+        if(a[1] == b[1]) {return 0};
+        });
 
-console.log(exports.charCount('an apple a day will keep the doctor away'));
+    return orderedCount;
+};
